34b03b2584288589b59b04bcf1763f38e42661da
Zohaib Shahid <bsef21m008@pucit.edu.pk>
2024-05-07 18:36:49 +0500

    Initial commit

---
+++ (Post mid Lec 5) 7-May-2024/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ProductRepository productsRepository = new ProductRepository();
+            List<Product> products = productsRepository.GetAll();
+            return View(products);
+        }
+
+        public ViewResult Add() { 
+        return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string Name, string Description) {
+
+            Product p = new Product {Name=Name, Description=Description };
+           ProductRepository productRepository = new ProductRepository();
+            productRepository.Add(p);
+            return RedirectToAction("Index","Product");
+        }
+    }
+}
